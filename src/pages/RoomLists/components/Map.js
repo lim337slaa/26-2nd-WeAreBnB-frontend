f2e8f1e75f724a35cecdb5233c8eff0d6f573d63
Summer Kim <sunmer6@gmail.com>
2021-11-16 17:40:25 +0900

    Add: map API 적용

--- src/pages/RoomLists/components/Map.js
+++ src/pages/RoomLists/components/Map.js
@@ -1,23 +1,89 @@
-import React from 'react';
+/* global google */
+import React, { useState, useEffect, useRef } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
 import styled from 'styled-components';
 
-function Map() {
+const Map = () => {
+  const [locations, setLocations] = useState([]);
+
+  const loader = useRef(null);
+  const map = useRef(null);
+  const marker = useRef(null);
+  const infoWindow = useRef(null);
+  const mapBoxRef = useRef();
+
+  const mapOptions = {
+    center: { lat: 41.3954, lng: 2.162 },
+    zoom: 12,
+  };
+
+  useEffect(() => {
+    loader.current = new Loader({
+      apiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+      version: 'weekly',
+    });
+
+    fetch('/data/Locations.json')
+      .then(res => res.json())
+      .then(data => setLocations(data));
+
+    loader.current.load().then(() => {
+      map.current = new google.maps.Map(mapBoxRef.current, mapOptions);
+      infoWindow.current = new google.maps.InfoWindow();
+      marker.current = locations.map(item => {
+        const { lat, lng, name } = item.location;
+        return new google.maps.Marker({
+          position: { lat: lat, lng: lng }, // 백엔드와 통신 시 latitude, longintude로 들어오게 될 예정이어서 줄이지 않고 일단 놔두었습니다.
+          map: map.current,
+          title: `${name}`,
+          label: `${name}`,
+          optimized: false,
+        });
+      });
+      return marker;
+    });
+
+    const script = document.createElement('script');
+    script.src = { loader };
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <MapBox>
-      <GoogleMap>맵자리</GoogleMap>
-    </MapBox>
+    <MapLoad>
+      <Box
+        ref={mapBoxRef}
+        id="map"
+        onClick={() => {
+          infoWindow.current.close();
+          infoWindow.current.setContent(
+            marker.current.map(marker && (marker => marker.getTitle()))
+          );
+          infoWindow.current.open(
+            marker.current.map(marker && (marker => marker.getMap())),
+            marker.current
+          );
+        }}
+      />
+    </MapLoad>
   );
-}
+};
 
 export default Map;
 
-const MapBox = styled.div`
+const MapLoad = styled.div`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
   width: 100%;
   height: 100%;
-  border: 1px solid green;
 `;
 
-const GoogleMap = styled.div`
-  width: 800px;
-  height: 1650px;
+const Box = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.borderColor};
 `;
