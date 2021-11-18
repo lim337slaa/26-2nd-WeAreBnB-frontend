f655c71de8e98997342b12872108709cb216c78f
lim337slaa <lim337slaa@hanmail.net>
2021-11-18 16:54:02 +0900

    메인페이지 레이아웃 완료

---
+++ src/pages/Main/Component/City.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function City(props) {
+  const { id, key, src, title, subtitle } = props;
+  return (
+    <div>
+      <Container backgroundcolor={props.backgroundcolor}>
+        <ImgContainer>
+          <img alt="error" src={src} />
+        </ImgContainer>
+        <subContainer>
+          <Text>{title}</Text>
+          <SubText>{subtitle}</SubText>
+        </subContainer>
+      </Container>
+    </div>
+  );
+}
+
+export default City;
+
+const Container = styled.div`
+  width: 300px;
+  height: 400px;
+  background-color: ${props => props.backgroundcolor};
+  border-radius: 15px;
+`;
+
+const ImgContainer = styled.div`
+  width: 300px;
+  height: 200px;
+  overflow: hidden;
+  img {
+    width: 300px;
+    height: 200px;
+  }
+
+  border-radius: 15px;
+`;
+
+const Text = styled.div`
+  width: 100px;
+  height: 75px;
+
+  margin: 30px 0px 0px 20px;
+  font-size: 36px;
+  color: white;
+`;
+
+const SubText = styled.div`
+  margin: 0px 0px 0px 20px;
+  font-size: 20px;
+  color: white;
+`;
