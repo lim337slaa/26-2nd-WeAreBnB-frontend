db608236ae75843719667acb390b84f3dee7b39d
Summer Kim <sunmer6@gmail.com>
2021-11-25 14:33:58 +0900

    modify: airbnb library initialize

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import Router from './Router';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
+import 'rheostat/initialize';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
 
 ReactDOM.render(
   <>
